2c7b9ea2ca1c871d59849d0cc3d43cc08fe1d2c3
Wesley <wspencer3295@yahoo.com>
2020-12-30 18:25:59 -0700

    +GET team/id +GET PATCH DELETE team/id +edit & show erb

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -29,6 +29,43 @@ class TeamsController < ApplicationController
         erb :'/teams/index'
     end
     
-   
+    get '/teams/:id' do
+        find_team
+        redirect_if_team_not_found
+        erb :'/teams/show'
+      end
+
+    get '/teams/:id/edit' do
+        find_team
+        redirect_if_team_not_found
+        erb :'teams/edit'
+    end
+
+    patch '/teams/:id' do
+        find_team
+        redirect_if_team_not_found
+        if @team.update(params[:user])
+            redirect "/teams/#{@team.id}"
+        else
+            redirect "/teams/#{@team.id}/edit"
+        end
+    end
+
+    delete '/teams/:id' do
+        find_team
+        if @team
+            @team.destroy
+            redirect "/teams"
+        end
+    end
+
+    private
+    def find_team
+        @team = Team.find_by_id(params[:id])
+    end
+    
+    def redirect_if_team_not_found
+        redirect "/teams" unless @team
+    end
 
 end
