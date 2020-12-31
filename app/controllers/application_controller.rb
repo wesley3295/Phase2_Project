ee130b455e0e5733a951b5589f8880f67910364b
Wesley <wspencer3295@yahoo.com>
2020-12-30 17:43:43 -0700

    + helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,21 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      
+    end
+    
+    def logged_in?
+      !!current_user
+    end
+
+    def logout
+      session.clear
+    end
+    
+  end
+
 end
