87508845cc65d07ff700cdc423d3a5b4c85b1e71
Wesley <wspencer3295@yahoo.com>
2020-12-30 18:02:17 -0700

    + GET champions/id

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -3,5 +3,16 @@ class ChampionsController < ApplicationController
     get '/champions' do
         erb :'/champions/index'
     end
-   
+
+    get '/champions/:id' do
+        find_champion
+        @stats = @champ.attributes.except("id")
+        erb :'champions/show'
+    end
+
+    private
+    def find_champion
+        @champ = Champion.find_by_id(params[:id])
+    end
+
 end
