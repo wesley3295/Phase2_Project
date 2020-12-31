8fd377625815d0eff7e09eaa0a80566eaa3af1e0
Wesley <wspencer3295@yahoo.com>
2020-12-30 18:06:36 -0700

    + GET POST  sign up & signup.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
 
-  
+    get '/signup' do
+        erb :'users/signup'
+    end
+    
+    post '/signup' do
+        user = User.new(params[:user])
+        if user.save
+            session[:user_id] = user.id
+            redirect "/teams"
+        else
+            redirect "/signup"
+        end
+    end
+
+    
         
 end
